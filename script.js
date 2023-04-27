6bf1e45cf619d81ae8f41f4a086a3aa230d85fe8
Alisson Augusto Rodrigues Sales <alissonsales2010@hotmail.com>
2023-04-27 15:51:55 -0300

    entregas4e1

--- script.js
+++ script.js
@@ -1,42 +1,38 @@
 // Implemente aqui as funções
 
 function validAbs(abs) {
-        if (abs >= 41){
-            return true
-        } 
-        return false 
+        return abs >= 41    
 }
 function validHeight(height, gender){
-    if (height >= 1.70 && gender === "male"){
-        return true
-    } else if (height >= 1.60 && gender === "female"){
-        return true
+    if (gender === "male"){
+        return height >= 1.70
     }
-    return false
+    if (gender === "female"){
+        return height >= 1.60
+    }
+    
 }
 function validSwim (swimTime, swimDistance, diveTime){
-    if (swimTime <= 60 && swimDistance >= 100 || diveTime <=30){
-        return true
-    }
-    return false
+    return (swimTime <= 60 && swimDistance >= 100 || diveTime <=30)
+    
 }
-function validBarTest (barReps, barTime, gender){
-    if (barReps >= 6 || barTime >= 15 && gender === "male"){
-    return true
-    } else if (barReps >= 5 || barTime >= 12 && gender === "female"){
-        return true
+function validBar (barReps, barTime, gender){
+    if (gender === "male"){
+    return barReps >= 6 || barTime >= 15
+    } if (gender === "female"){
+        return barReps >= 5 || barTime >= 12
     }
-    return false
+    
 }
 function validRun (runTime, runDistance, gender){
-    if (runTime <= 720 && runDistance >= 3000 || runTime <= 1200 && runDistance >= 5000 && gender === "male"){
-        return true
-    } else if (runTime <= 900 && runDistance >= 4000 || runTime <= 1320 && runDistance >= 6000 && gender === "female"){
-        return true
+    if (gender === "male"){
+        return runTime <= 720 && runDistance >= 3000 || runTime <= 1200 && runDistance >= 5000
+    } if (gender === "female"){
+        return runTime <= 900 && runDistance >= 4000 || runTime <= 1320 && runDistance >= 6000
     }
-    return false
+    
 }
-function areCandidateResultsValid(gender, height, barReps, barTime, abs, runDistanc, runTime, swimDistance, swimTime, diveTime){
+function areCandidateResultsValid(gender, height, barReps, barTime, abs, runDistance, runTime, swimDistance, swimTime, diveTime){
     const verifyAbs = validAbs(abs)
     const verifyHeight = validHeight(height, gender)
     const verifySwim = validSwim(swimTime, swimDistance, diveTime)
@@ -44,10 +40,7 @@ function areCandidateResultsValid(gender, height, barReps, barTime, abs, runDist
     const verifyRun = validRun(runTime, runDistance, gender)
 
 
-    if (verifyAbs && verifyHeight && verifySwim && verifyBarTest && verifyRun){
-        return true
-    }
-    return false
+   return verifyAbs && verifyHeight && verifySwim && verifyBarTest && verifyRun
     
 }
 
@@ -56,7 +49,7 @@ function areCandidateResultsValid(gender, height, barReps, barTime, abs, runDist
 
 // Chamando a função com os parametros recebidos via script de teste
 // e atribuindo a variavel que devera ser mostrada no console
-const areCandidateResultsValid = areCandidateResultsValid(
+const areCandidateValid = areCandidateResultsValid(
     process.argv[2], // Genero
     process.argv[3], // Altura
     process.argv[4], // Repetições de barra
@@ -68,10 +61,7 @@ const areCandidateResultsValid = areCandidateResultsValid(
     process.argv[10], // Tempo total de nado em segundos
     process.argv[11], // Tempo total de mergulho em segundos
 );
-//if (gender === "male" && height >= 1.70 && (barReps >= 6 || barSeconds >= 15) && abs >= 41 &&
-//(runDistance >= 3000 && runTime <= 720 || runDistance >= 5000 && runTime <= 1200) && (swimDistance >= 100 && swimTime <= 60 || diveTime <= 30)){
-//    passed=true
-//} else if(gender === "female" && height >= 1.60 && (barReps >= 5 || barSeconds >= 12) && abs >= 41 &&
-//(runDistance >= 4000 && runTime <= 900 || runDistance >= 6000 && runTime <= 1320) && (swimDistance >= 100 && swimTime <= 60 || diveTime <= 30)){
-//    passed=true
-// Chame aqui a função que mostra o resultado no console
+function showMessage(message){
+    console.log(message.toString().toUpperCase())
+}
+showMessage(areCandidateValid)
